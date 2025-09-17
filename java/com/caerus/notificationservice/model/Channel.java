923c8690950663cab95bf6e33290a76474365a5b
Kaushik Nandy <CITC_u0005@caerusitconsulting.com>
2025-09-17 10:49:05 +0530

    delete unused

--- java/com/caerus/notificationservice/model/Channel.java
+++
@@ -1,2 +0,0 @@
-package com.caerus.notificationservice.model;
-public enum Channel { EMAIL, SMS, WHATSAPP, IN_APP }
--- java/com/caerus/notificationservice/service/Sender.java
+++
@@ -1,5 +0,0 @@
-package com.caerus.notificationservice.service;
-public interface Sender {
-    void send(String to, String subject,  String message) throws Exception;
-    //void sendEmail(String to,String subject,String body)throws Exception;
-}
